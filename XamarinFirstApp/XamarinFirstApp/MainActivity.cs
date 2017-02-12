6d05e9482e8074af061f563e9dadc5e355874bca
Will <williampsena@gmail.com>
2017-02-12 20:27:49 -0200

    Exercícios Maratona Xamarin

---
+++ XamarinFirstApp/XamarinFirstApp/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinFirstApp
+{
+    [Activity(Label = "XamarinFirstApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
